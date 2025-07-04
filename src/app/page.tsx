d91d2bdf536a37852c03d8d9fec11d5c7b1c1dfb
Raul Maglione <raulmms192000@gmail.com>
2025-07-04 15:22:06 +0000

    i have detected a bug maybe, because when i drag an arca down it functio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,11 @@ export default function Home() {
   }
 
   const sensors = useSensors(
-    useSensor(PointerSensor),
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
     useSensor(TouchSensor, {
       activationConstraint: {
         delay: 250,
